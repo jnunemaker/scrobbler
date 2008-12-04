5ed76bce91c0dc92a4ccb95d9b5efaa50693f6d1
Jonathan Rudenberg <jon335@gmail.com>
2008-12-03 22:08:39 -0500

    missed REST mock in previous commit

--- test/mocks/rest.rb
+++ test/mocks/rest.rb
@@ -1,25 +1,50 @@
 require File.dirname(__FILE__) + '/../../lib/scrobbler/rest'
+require 'digest/md5'
 
 module Scrobbler
   module REST
   	class Connection
   	  # reads xml fixture file instead of hitting up the internets
   	  def request(resource, method = "get", args = nil)
-  	    pieces = resource.split('/')
-  	    pieces.shift
-  	    pieces.shift
-  	    folder = pieces.shift
-  	    file   = pieces.last[0, pieces.last.index('.xml')]
-  	    base_pieces = pieces.last.split('?')
+  	    @now_playing_url = 'http://62.216.251.203:80/nowplaying'
+  	    @submission_url = 'http://62.216.251.205:80/protocol_1.2'
+  	    @session_id = '17E61E13454CDD8B68E8D7DEEEDF6170'
   	    
-  	    file = if base_pieces.size > 1
-  	      # if query string params are in resource they are underscore separated for filenames
-  	      base_pieces.last.split('&').inject("#{file}_") { |str, pair| str << pair.split('=').join('_') + '_'; str }.chop!
-	      else
-	        file
-        end
+  	    if @base_url == Scrobbler::API_URL
+    	    pieces = resource.split('/')
+    	    pieces.shift
+    	    pieces.shift
+    	    folder = pieces.shift
+    	    file   = pieces.last[0, pieces.last.index('.xml')]
+    	    base_pieces = pieces.last.split('?')
   	    
-  	    File.read(File.dirname(__FILE__) + "/../fixtures/xml/#{folder}/#{file}.xml")
+    	    file = if base_pieces.size > 1
+    	      # if query string params are in resource they are underscore separated for filenames
+    	      base_pieces.last.split('&').inject("#{file}_") { |str, pair| str << pair.split('=').join('_') + '_'; str }.chop!
+  	      else
+  	        file
+          end
+  	    
+    	    File.read(File.dirname(__FILE__) + "/../fixtures/xml/#{folder}/#{file}.xml")
+  	    elsif @base_url == Scrobbler::AUTH_URL
+          if args[:hs] == "true" && args[:p] == Scrobbler::AUTH_VER.to_s && args[:c] == 'rbs' &&
+             args[:v] == Scrobbler::Version.to_s && args[:u] == 'chunky' && !args[:t].blank? &&
+             args[:a] == Digest::MD5.hexdigest('7813258ef8c6b632dde8cc80f6bda62f' + args[:t])
+            
+            "OK\n#{@session_id}\n#{@now_playing_url}\n#{@submission_url}"
+          end
+        elsif @base_url == @now_playing_url
+          if args[:s] == @session_id && !args[:a].blank? && !args[:t].blank? && !args[:b].blank? &&
+             !args[:n].blank?
+             'OK'
+          end           
+        elsif @base_url == @submission_url
+          if args[:s] == @session_id && !args['a[0]'].blank? && !args['t[0]'].blank? &&
+             !args['i[0]'].blank? && !args['o[0]'].blank? && !args['l[0]'].blank? && !args['b[0]'].blank?
+             'OK'
+          end
+	      end
+	      
 	    end
 	  end
   end
